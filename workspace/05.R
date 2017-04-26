66b0395823bfa6f2f742fce3b307d8c3cc66baa1
csiu <celia.sy.siu@gmail.com>
2017-04-26 11:49:44 -0700

    include link so people do not need to type this in

--- workspace/05.R
+++ workspace/05.R
@@ -13,3 +13,4 @@
 
 #' ## Inspecting data frames
 #' ### Realistic example
+# gapminder <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv")
